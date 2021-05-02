978d9d27d4ea2549bfc4cb40e31918660b2873ed
Nico Rithner <nicorithner@gmail.com>
2021-05-02 10:40:58 -0600

    Update Saturday

--- app.js
+++ app.js
@@ -60,3 +60,19 @@ if(loggedInUser) {
   console.log('Please log in');
 }
 
+let psswrd = 'chicken Gal';
+if(psswrd.length >= 8 && psswrd.indexOf(' ') === -1) {
+  console.log('Valid Password');
+} else {
+  console.log('Invalid Password');
+}
+
+let numero = 3;
+
+if(numero >= 1 && numero <= 10) {
+  console.log('Number is between 1 and 10');
+} else {
+  console.log('Please guess a number between 1 and 10');
+}
+
+
