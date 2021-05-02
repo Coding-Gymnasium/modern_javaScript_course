79b580572c5703c324ba843705498f17657ac5c1
Nico Rithner <nicorithner@gmail.com>
2021-05-01 20:07:49 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// conditionals
+
+if (1 === 1){
+  console.log("it's true");
+}
+
+let rating = 2;
+
+if(rating === 3) {
+  console.log('You are super start');
+} else if (rating === 2) {
+  console.log('Needs improvement');
+} else {
+  console.log('you suck');
+}
+  
+let num = 37;
+
+if(num % 2 !== 0) {
+  console.log('Odd number');
+}
+
+
+let highScore = 1430;
+let userScore = 1600;
+
+if (userScore >= highScore) {
+  console.log(`Congrats, you have the new high score of ${userScore}`);
+  highScore = userScore;
+} else {
+  console.log(`Good game. Your score of ${userScore} did not beat the high score of ${highScore}`);
+}
+
+let password = 'hello kitty';
+
+if (password.length >= 6) {
+  if (password.indexOf(' ') === -1) {
+    console.log('Valid Password');
+  } else {
+    console.log('Password is long enough, but cannot contain spaces');
+  }
+} else {
+  console.log('Password must be longer');
+}
+
+let mistery = 5;
+// let mistery = NaN;
+
+if (mistery) {
+  console.log('Truthy');
+} else {
+  console.log('Falsy');
+}
+
+let loggedInUser = null;
+
+if(loggedInUser) {
+  console.log('You are logged in');
+} else {
+  console.log('Please log in');
+}
+
